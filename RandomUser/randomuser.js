482e9d074f5b3cab9549d385fabcde363ffc110a
Yamileth Mendoza Cital <L21170016@culiacan.tecnm.mx>
2023-11-08 10:51:21 -0700

    Mejorando el Random User

--- RandomUser/randomuser.js
+++ RandomUser/randomuser.js
@@ -1,10 +1,37 @@
+var numero = 0;
+var cantidad = 0;
+var datos = [];
+
 async function apirandomuser(){
-    const respuesta = await fetch("https://randomuser.me/api/");
-    const datos = await respuesta.json();
+    numero = 0;
+    cantidad = document.getElementById("cantidad").value;
+    const respuesta = await fetch("https://randomuser.me/api?results="+cantidad);
+    datos = await respuesta.json();
+    muestra();
+}
+
+function anterior(){
+    numero--;
+    if(numero < 0){
+        numero = cantidad - 1;
+    }
+    muestra();
+}
+
+function siguiente(){
+    numero++;
+    if(numero == cantidad){
+        numero = 0;
+    }
+    muestra();
+}
+
+function muestra(){
+    console.log("Arreglo posición: "+numero);
     let foto = document.getElementById("foto");
     let nombre = document.getElementById("nombre");
     let telefono = document.getElementById("telefono");
-    foto.setAttribute("src",datos.results[0].picture.large);
-    nombre.innerHTML=datos.results[0].name.first+" "+datos.results[0].name.last;
-    telefono.innerHTML=datos.results[0].phone;
+    foto.setAttribute("src",datos.results[numero].picture.large);
+    nombre.innerHTML=datos.results[numero].name.first+" "+datos.results[numero].name.last;
+    telefono.innerHTML=datos.results[numero].phone;
 }
